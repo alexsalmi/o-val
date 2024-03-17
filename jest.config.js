0096b15edbc671bc10f750adc575815a54e2b9f3
alexsalmi <alexjfsalmi@gmail.com>
2024-03-17 11:46:06 -0400

    Add code coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,12 @@ export default {
     "^(\\.\\.?\\/.+)\\.js$": "$1",
     "classes/(.*).js": "<rootDir>/src/classes/$1",
     "rules/(.*).js": "<rootDir>/src/rules/$1"
-  }
+  },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "src/**/*",
+    "!src/types/*"
+  ]
 }
